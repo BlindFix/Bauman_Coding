f23cdac33dfa270337a575be3b8d5c85a78d884e
Blind <to_bee@icloud.com>
2021-12-03 19:39:07 +0300

    added polynom.c

--- Term_1/Algorithms_and_data_structures/Module_1(Learning_the_C_language)/polynom.c
+++ Term_1/Algorithms_and_data_structures/Module_1(Learning_the_C_language)/polynom.c
@@ -1,1 +1,24 @@
 #include <stdio.h>
+
+int	main(void)
+{
+	long	n;
+	long	x0;
+	long	res0 = 0;
+	long	res1 = 0;
+	long	x;
+
+	scanf(" %ld %ld", &n, &x0);
+	while (n > 0)
+	{
+		scanf(" %ld", &x);
+		res0 = (res0 * x0) + x;
+		res1 = (res1 * x0) + x * (n--);
+	}
+
+	scanf(" %ld", &x);
+	res0 = (res0 * x0) + x;
+
+	printf("%ld %ld", res0, res1);
+	return (0);
+}
