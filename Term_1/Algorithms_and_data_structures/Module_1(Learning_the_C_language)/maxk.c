cff58c5ce301e5d9d2eee8b18b9f23d7c2a10449
Blind <to_bee@icloud.com>
2022-01-25 20:34:09 +0300

    added maxk.c

---
+++ Term_1/Algorithms_and_data_structures/Module_1(Learning_the_C_language)/maxk.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	unsigned	n, k;
+
+	scanf(" %d", &n);					// cчитываем размер массива
+
+	long		arr[n], max_sum = 0, now_sum;
+
+	for(size_t i = 0; i < n; i++)		// заполняем массив
+		scanf(" %ld", &arr[i]);
+
+	scanf(" %d", &k);					// считываем размер k
+
+	for(size_t i = 0; i < k; i++)
+		max_sum += arr[i];
+
+	now_sum = max_sum;					// приравниваем значение счетчика нынешней суммы к значению суммы первых k элементов массива
+	for(size_t i = k; i < n; i++)
+	{
+		now_sum += -arr[i - k] + arr[i];// на каждой итерации из нынешней суммы вычитаем самый "старый" элемент и добавляем новый
+		if (now_sum > max_sum)
+			max_sum = now_sum;			// если нынешняя сумма стала больше максимальной, то это новая максимальная сумма
+	}
+
+	printf("%ld", max_sum);
+	return (0);
+}
